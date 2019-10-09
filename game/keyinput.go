cf69ea42dfdae16709142b75479679bc6cbb83e4
Tristan Goossens <d267316@edu.rocwb.nl>
2019-10-09 19:10:38 +0200

    added mainmenu control

--- game/keyinput.go
+++ game/keyinput.go
@@ -2,37 +2,38 @@ package trisnake
 
 import tl "github.com/JoelOtter/termloop"
 
-type direction int
-
-const (
-	up direction = iota
-	down
-	left
-	right
-)
-
 func (snake *Snake) Tick(event tl.Event) {
-	if event.Type == tl.EventKey { 
-		switch event.Key { 
+	if event.Type == tl.EventKey {
+		switch event.Key {
 		case tl.KeyArrowRight:
 			if snake.Direction != left {
 				snake.Direction = right
-			}	
+			}
 		case tl.KeyArrowLeft:
 			if snake.Direction != right {
 				snake.Direction = left
-			}	
+			}
 		case tl.KeyArrowUp:
 			if snake.Direction != down {
 				snake.Direction = up
-			}	
+			}
 		case tl.KeyArrowDown:
 			if snake.Direction != up {
 				snake.Direction = down
-			}	
+			}
 		}
 	}
 }
 
+func (game *Gameoptions) Tick(event tl.Event) {
+	if event.Type == tl.EventKey {
+		switch event.Key {
+		case tl.KeyEsc:
+			game.Started = true
+		}
+	}
 
-
+	if game.Started == true {
+		StartGame()
+	}
+}
