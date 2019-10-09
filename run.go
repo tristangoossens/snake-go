f37b5fc3237359b6eb000b3f41322706c44d5ed5
Tristan Goossens <tristan.goossens@idtrust.com>
2019-10-09 16:52:29 +0200

    added arena border and sidepanel into test

--- run.go
+++ run.go
@@ -13,22 +13,31 @@ const (
 
 type gamemode int
 
-func StartGame() {
-	
+var width int = 70
+var height int = 25
+
+func NewSidepanel() (*tl.Rectangle, *tl.Text) {
+	sidepanel := tl.NewRectangle(width+1,0,30,height, tl.ColorWhite)
+	titletext := tl.NewText(width +2, 1, "Score: 0", tl.ColorBlack, tl.ColorWhite)
+	return sidepanel, titletext
 }
 
 func main() {
 	game := tl.NewGame()
 
 	level := tl.NewBaseLevel(tl.Cell{
-		Bg: tl.ColorGreen,
-		Fg: tl.ColorBlack,
+		Bg: tl.ColorBlack,
 	})
 
 	snake := s.NewSnake()
-	level.AddEntity(snake)
+	arena := s.NewArena(width, height)
+	sidepanel, titletext := NewSidepanel()		
 
+	level.AddEntity(sidepanel)
+	level.AddEntity(titletext)
 	level.AddEntity(snake)
+	level.AddEntity(arena)
+
 	game.Screen().SetLevel(level)
 	game.Screen().SetFps(10)
 	game.Start()
