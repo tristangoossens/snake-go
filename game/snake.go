fb66633dc03478a741e876bda754cbf82de7c790
Tristan Goossens <tristan.goossens@idtrust.com>
2019-10-11 15:32:23 +0200

    added working food collision

--- game/snake.go
+++ game/snake.go
@@ -8,6 +8,7 @@ func NewSnake() *Snake {
 	snake := new(Snake)
 	snake.Entity = tl.NewEntity(5, 5, 1, 1)
 	snake.Direction = right
+	snake.Length = len(snake.Bodylength)
 	snake.Bodylength = []Coordinates{
 		{1, 6},
 		{2, 6},
@@ -21,11 +22,11 @@ func (snake *Snake) Head() *Coordinates {
 	return &snake.Bodylength[len(snake.Bodylength)-1]
 }
 
-func (snake *Snake) isCollidingWithBorder() bool {
+func (snake *Snake) BorderCollison() bool {
 	return arena.Contains(*snake.Head())
 }
 
-func (snake *Snake) isCollidingWithFood() bool {
+func (snake *Snake) FoodCollision() bool {
 	return food.Contains(*snake.Head())
 }
 
@@ -42,23 +43,31 @@ func (snake *Snake) Draw(screen *tl.Screen) {
 		nHead.X++
 	}
 
-	snake.Bodylength = append(snake.Bodylength[1:], nHead)
+	if snake.FoodCollision() {
+		switch food.Emoji {
+		case '🐁':
+			UpdateScore(5)
+		case 'R':
+			UpdateScore(5)
+		default:
+			UpdateScore(1)
+			snake.Bodylength = append(snake.Bodylength, nHead)
+		}
+		food.MoveFood()
+	} else {
+		snake.Bodylength = append(snake.Bodylength[1:], nHead)
+	}
 
 	snake.SetPosition(nHead.X, nHead.Y)
 
-	if snake.isCollidingWithBorder() {
+	if snake.BorderCollison() {
 		Gameover()
-	}
-
-	if snake.isCollidingWithFood() {
-		score++
-		NewSidepanel()
 	}
 
 	for _, c := range snake.Bodylength {
 		screen.RenderCell(c.X, c.Y, &tl.Cell{
 			Fg: tl.ColorWhite,
-			Ch: '◻',
+			Ch: '▒',
 		})
 	}
 }
