b0120f9682a09f9fd7389bba0e1a2dd9437c26d5
Tristan Goossens <d267316@edu.rocwb.nl>
2019-10-11 20:11:29 +0200

    added documentation for functions

--- game/arena.go
+++ game/arena.go
@@ -2,19 +2,25 @@ package trisnake
 
 import tl "github.com/JoelOtter/termloop"
 
+// NewArena will create a new arena with the arena with and arena height given when this function was called in game.go
+// This function will create a arena using the arena struct that can be found in the types.go file.
 func NewArena(w, h int) *Arena {
 	arena := new(Arena)
+	// Width and height of the arena are decresed by one to add corners on the arena border.
 	arena.Width = w - 1
 	arena.Height = h - 1
+	// Each arena cell will have a width and heigth of 1.
 	arena.Entity = tl.NewEntity(1, 1, 1, 1)
-
+	// Creates a map of coordinates.
 	arena.ArenaBorder = make(map[Coordinates]int)
 
+	// This for loop will create the top and bottom borders
 	for x := 0; x < arena.Width; x++ {
 		arena.ArenaBorder[Coordinates{x, 0}] = 1
 		arena.ArenaBorder[Coordinates{x, arena.Height}] = 1
 	}
 
+	// This for loop will create the left and right borders
 	for y := 0; y < arena.Height+1; y++ {
 		arena.ArenaBorder[Coordinates{0, y}] = 1
 		arena.ArenaBorder[Coordinates{arena.Width, y}] = 1
@@ -22,17 +28,16 @@ func NewArena(w, h int) *Arena {
 	return arena
 }
 
+// Contains checks if the arenaborder map contains the coordinates of the snake, if so this will return true.
 func (arena *Arena) Contains(c Coordinates) bool {
 	_, exists := arena.ArenaBorder[c]
 	return exists
 }
 
+// Draw is a termloop function that will draw out the arena border, this is called when the game has started.
 func (arena *Arena) Draw(screen *tl.Screen) {
+	// This for loop will range ArenaBorder containing the coordinates of the arenaborder and will print them out on the screen.
 	for i := range arena.ArenaBorder {
-		if arena == nil {
-			return
-		}
-
 		screen.RenderCell(i.X, i.Y, &tl.Cell{
 			Bg: tl.ColorWhite,
 		})
--- game/food.go
+++ game/food.go
@@ -7,93 +7,107 @@ import (
 	tl "github.com/JoelOtter/termloop"
 )
 
-func NewFood() *Food {
-	var insideborder int = 1
-	rand.Seed(time.Now().UnixNano())
+// Variable insideborderW and insideborderH are variables consisting of the arenawidth and height and subtract both with 1
+// in order to account for the arena border.
+var insideborderW int = arenawidth - 1
+var insideborderH int = arenaheight - 1
 
+// NewFood will create a new piece of food, this will only happen once when the game has started.
+func NewFood() *Food {
 	food := new(Food)
-	food.Foodposition.X = rand.Intn(arenawidth - insideborder + insideborder - 1)
-	food.Foodposition.Y = rand.Intn(arenaheight - insideborder + insideborder - 1)
-	food.Entity = tl.NewEntity(food.Foodposition.X, food.Foodposition.Y, 1, 1)
-	if utf8support {
-		food.Emoji = RandomFoodUTF8()
-	} else {
-		food.Emoji = RandomFood()
-	}
+	// Create a new entity food with a standard position and 1x1 size
+	food.Entity = tl.NewEntity(1, 1, 1, 1)
+	// Call function MoveFood to move the food to a random position.
+	food.MoveFood()
+
 	return food
 }
 
+// MoveFood moves the food into a new random position.
 func (food *Food) MoveFood() {
-	insideborderW := arenawidth - 1
-	insideborderH := arenaheight - 1
 
+	// Calls the RandomInsideArena function to make sure that the foods spawns inside the arena.
 	NewX := RandomInsideArena(insideborderW, 1)
 	NewY := RandomInsideArena(insideborderH, 1)
 
+	// Changes the X and Y coordinates of the food.
 	food.Foodposition.X = NewX
 	food.Foodposition.Y = NewY
 
+	// Checks for UTF-8 Support to choose if the system supports Unicode.
 	if utf8support {
+		// The system supports Unicode and will pick a random emoji as a food icon.
 		food.Emoji = RandomFoodUTF8()
 	} else {
+		// The system does not support Unicode and will use the regular ASCII-charset to pick a random food emoji.
 		food.Emoji = RandomFood()
 	}
 
+	// Set the new position of the food.
 	food.SetPosition(food.Foodposition.X, food.Foodposition.Y)
 }
 
+// RandomFood will use the ASCII-charset to pick a random rune from the slice and print it out as food.
 func RandomFood() rune {
+	// This slice contains all of the possible food icons.
 	emoji := []rune{
 		'R', // Favourite dish, extra points!!! 😋
-		'■',
-		'■',
-		'■',
-		'■',
-		'■',
-		'■',
-		'■',
-		'■',
-		'■',
-		'■',
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
+		'■', // 1 point
 		'S', // You do not want to eat the skull 💀
 	}
 
 	rand.Seed(time.Now().UnixNano())
 
+	// Return a random rune picked from the slice
 	return emoji[rand.Intn(len(emoji))]
 }
 
+// RandomFoodUTF8 will use the Unicode charset to pick a random rune from the slice and print it out as food.
 func RandomFoodUTF8() rune {
+	// This slice contains all of the possible food icons.
 	emoji := []rune{
 		'🐁', // Favourite dish, extra points!!! 😋
-		'🍔',
-		'🍆',
-		'🍑',
-		'🍏',
-		'🍊',
-		'🍝',
-		'🍌',
-		'🍞',
-		'🍟',
-		'🍎',
+		'🍔', // Hamburger, 1 point
+		'🍆', // Eggplant, 1 point
+		'🍑', // Peach, 1 point
+		'🍏', // Green apple, 1 point
+		'🍊', // Orange, 1 point
+		'🍝', // Spaghetti, 1 point
+		'🍌', // Banana, 1 point
+		'🍞', // Bread, 1 point
+		'🍟', // Fries, 1 point
+		'🍎', // Apple, 1 point
 		'💀', // You do not want to eat the skull 💀
 	}
 
 	rand.Seed(time.Now().UnixNano())
 
+	// Return a random rune picked from the slice
 	return emoji[rand.Intn(len(emoji))]
 }
 
+// Draw will print out the food on the screen.
 func (food *Food) Draw(screen *tl.Screen) {
 	screen.RenderCell(food.Foodposition.X, food.Foodposition.Y, &tl.Cell{
 		Ch: food.Emoji,
 	})
 }
 
+// Contains checks if food contains the coordinates, if so this will return a bool.
 func (food *Food) Contains(c Coordinates) bool {
 	return c.X == food.Foodposition.X && c.Y == food.Foodposition.Y
 }
 
+// RandomInsideArena will the minimal, which is just inside the border and the maximal, being the arena width or height.
 func RandomInsideArena(iMax int, iMin int) int {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(iMax-iMin) + iMin
