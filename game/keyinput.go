67df508878f726261ddeb8ddb8f3928c0e466b80
Tristan Goossens <tristan.goossens@idtrust.com>
2019-10-23 16:47:04 +0200

    added keyinputs to keyinput.go

--- game/keyinput.go
+++ game/keyinput.go
@@ -1,6 +1,11 @@
 package trisnake
 
-import tl "github.com/JoelOtter/termloop"
+import (
+	"fmt"
+
+	tl "github.com/JoelOtter/termloop"
+	tb "github.com/nsf/termbox-go"
+)
 
 // Tick listens for a keypress and then returns a direction for the snake.
 func (snake *Snake) Tick(event tl.Event) {
@@ -35,6 +40,59 @@ func (snake *Snake) Tick(event tl.Event) {
 				// Changes the direction of the snake to up.
 				snake.Direction = down
 			}
+		}
+	}
+}
+
+// Tick is a method for the gameoverscreen wich listens for either a restart or a quit input from the user.
+func (gos *Gameoverscreen) Tick(event tl.Event) {
+	// Check if the event is a key event.
+	if event.Type == tl.EventKey {
+		switch event.Key {
+		// If the key pressed is backspace the game will restart!!
+		case tl.KeyHome:
+			// Will call the RestartGame function to restart the game.
+			RestartGame()
+		case tl.KeyDelete:
+			// Will end the game using a fatal log. This uses the termbox package as termloop does not have a function like that.
+			tb.Close()
+		}
+	}
+}
+
+// Tick will listen for a keypress to initiate the game.
+func (ts *Titlescreen) Tick(event tl.Event) {
+	// Checks if the event is a keypress event and the key pressed is the enter key.
+	if event.Type == tl.EventKey {
+		if event.Key == tl.KeyEnter {
+			// Creates a new baselevel ffor the snake game.
+			gs = NewGamescreen()
+			// Sets the gamelevel to the snakegame! 🐍
+			sg.Screen().SetLevel(gs)
+		}
+		if event.Key == tl.KeyInsert {
+			gop := NewOptionsscreen()
+			sg.Screen().SetLevel(gop)
+		}
+	}
+}
+
+// Tick will listen for a keypress to initiate the game.
+func (gop *Gameoptionsscreen) Tick(event tl.Event) {
+	// Checks if the event is a keypress event and the key pressed is the enter key.
+	if event.Type == tl.EventKey {
+		switch event.Key {
+		case tl.KeyF1:
+			ts.GameDifficulty = easy
+			gop.CurrentDifficultyText.SetText(fmt.Sprintf("Current difficulty: Easy"))
+
+		case tl.KeyF2:
+			ts.GameDifficulty = normal
+			gop.CurrentDifficultyText.SetText(fmt.Sprintf("Current difficulty: Normal"))
+
+		case tl.KeyF3:
+			ts.GameDifficulty = hard
+			gop.CurrentDifficultyText.SetText(fmt.Sprintf("Current difficulty: Hard"))
 		}
 	}
 }
